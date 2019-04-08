47ab44680c627624f4d66727a45c27f240e2fc78
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-04-08 15:26:23 -0500

    initia commit and project setup folders and files

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Card = ()=>{
+return (
+     <div>
+          <img  alt = "robots" src="https://robohash.org/you.png" />
+          <div>
+               <h2>Robot Name</h2>
+               <p>ARobot@gmail.com</p>
+          </div>
+     </div>
+)
+}
+export default Card
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import serviceWorker from './serviceWorker'
+import './index.css';
+import Card from './Card'
+import 'tachyons';
+ReactDOM.render(<Card />, document.getElementById('root'));
